11c1e614592d2256b001c3d91e9e627447dd81e4
MshSiam <siyamkenway@gmail.com>
2022-09-22 15:15:25 +0600

    project image aded

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,11 +3,14 @@ import React from "react"
 import "./Projects.css"
 import { AiOutlineProject } from "react-icons/ai"
 import { FaRegSmileWink } from "react-icons/fa"
-import ProjectCard from "./ProjectCard"
+import techmart from "../../images/website/techmart.png"
+import mall from "../../images/website/mall.png"
+import emart from "../../images/website/emart.png"
+import bike from "../../images/website/bike.png"
+import food from "../../images/website/food.png"
+import realstate from "../../images/website/realstate.png"
 
 const Projects = () => {
-  const projects = [1, 2, 3, 4]
-
   return (
     <div className="projects">
       <Typography variant="h3">
@@ -16,9 +19,12 @@ const Projects = () => {
 
       <div className="projectsWrapper">
         <>
-          <a href="" className="projectCard" target="_blank">
+          <a
+            href="https://msh-siam-ecommerce.netlify.app"
+            className="projectCard"
+            target="_blank">
             <div>
-              <img src="" alt="project" />
+              <img src={techmart} alt="project" />
               <Typography variant="h5">projectTitle</Typography>
             </div>
             <div>
@@ -29,9 +35,12 @@ const Projects = () => {
           </a>
         </>
         <>
-          <a href="" className="projectCard" target="_blank">
+          <a
+            href="https://mall-heroku-mshsiam.netlify.app"
+            className="projectCard"
+            target="_blank">
             <div>
-              <img src="" alt="project" />
+              <img src={mall} alt="project" />
               <Typography variant="h5">projectTitle</Typography>
             </div>
             <div>
@@ -42,9 +51,60 @@ const Projects = () => {
           </a>
         </>
         <>
-          <a href="" className="projectCard" target="_blank">
+          <a
+            href="https://emart-mshsiam.netlify.app"
+            className="projectCard"
+            target="_blank">
             <div>
-              <img src="" alt="project" />
+              <img src={emart} alt="project" />
+              <Typography variant="h5">projectTitle</Typography>
+            </div>
+            <div>
+              <Typography variant="h4">About Project</Typography>
+              <Typography>projectDescreption</Typography>
+              <Typography variant="h6">Tech Stack : technologies</Typography>
+            </div>
+          </a>
+        </>
+        <>
+          <a
+            href="https://bike-website-ac530.web.app/"
+            className="projectCard"
+            target="_blank">
+            <div>
+              <img src={bike} alt="project" />
+              <Typography variant="h5">projectTitle</Typography>
+            </div>
+            <div>
+              <Typography variant="h4">About Project</Typography>
+              <Typography>projectDescreption</Typography>
+              <Typography variant="h6">Tech Stack : technologies</Typography>
+            </div>
+          </a>
+        </>
+        <>
+          <a
+            href="https://hot-onion-msh-siam.netlify.app"
+            className="projectCard"
+            target="_blank">
+            <div>
+              <img src={food} alt="project" />
+              <Typography variant="h5">projectTitle</Typography>
+            </div>
+            <div>
+              <Typography variant="h4">About Project</Typography>
+              <Typography>projectDescreption</Typography>
+              <Typography variant="h6">Tech Stack : technologies</Typography>
+            </div>
+          </a>
+        </>
+        <>
+          <a
+            href="https://real-state-house-siam.netlify.app"
+            className="projectCard"
+            target="_blank">
+            <div>
+              <img src={realstate} alt="project" />
               <Typography variant="h5">projectTitle</Typography>
             </div>
             <div>
